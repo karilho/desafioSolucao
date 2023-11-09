8de80947ceab5aa8aceb612915d28a867bd7411a
lucalmeida <lucalmeida@brasilseg.com.br>
2023-11-09 20:49:48 -0300

    Desafio parte 1 reformulado e funcionando

--- Leitor.java
+++ Leitor.java
@@ -2,24 +2,33 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Leitor {
 
     // METODO 1 - > LER TEXT E PRINTAR NA TELA.
-    public void LerTXT () throws IOException {
-        BufferedReader leitorDePerguntas = new BufferedReader(new FileReader("formulario.txt"));
-        System.out.println("DEU BOM, o arquivo foi encontrado e lido!");
+    public List<String> LerTXT() {
 
-        String linhaAserLida;
+        List<String> listaDePerguntas = new ArrayList<>();
 
-        while ((linhaAserLida = leitorDePerguntas.readLine()) != null ) {
-            System.out.println(linhaAserLida);
+        // Tente este bloco de código
+        try {
+            BufferedReader leitorDePerguntas = new BufferedReader(new FileReader("formulario.txt"));
+            System.out.println("DEU BOM, o arquivo foi encontrado e lido!");
+
+            String linhaAserLida;
+
+            while ((linhaAserLida = leitorDePerguntas.readLine()) != null) {
+                // Adicionada cada pergunta por vez no loop ao array listaDePerguntas
+                listaDePerguntas.add(linhaAserLida);
+
+            }
+        } catch (Exception e) {
+            // SE DER MERDA FAÇA ISSO
+            e.printStackTrace();
         }
+
+        return listaDePerguntas;
     }
-    // METODO 2 -> LER CSV
-
-    // METODO 3 -> LER IMAGEM
-
-
-
 }
--- Main.java
+++ Main.java
@@ -1,10 +1,23 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("TESTE");
-        Leitor perguntasLidas = new Leitor();
-        perguntasLidas.LerTXT();
+        Leitor leitorArquivo = new Leitor();
+
+        List<String> perguntasLidasDoTXT = leitorArquivo.LerTXT();
+
+        //   CONTADOR        ENQUANTO                   FAÇA ISSO
+        for (int i = 0; i < perguntasLidasDoTXT.size();   i++  )
+        {
+            System.out.println(perguntasLidasDoTXT.get(i));
+        }
+        // PARA CADA PERGUNTA DO PERGUNTASLIDAS DO TXT
+        for (String pergunta : perguntasLidasDoTXT){
+            //FACA ISSO.
+            System.out.println(pergunta);
+        }
     }
 }
